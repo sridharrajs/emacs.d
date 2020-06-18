58931edf94bd894a1a5363fc5802c5c3694af5a1
sridharrajs <sridhar@aghenam.com>
2020-06-18 13:39:26 +0530

    adding shortcut for markdown preview

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,6 +5,15 @@
 
 ;;; Code:
 
+;; for markdown-preview to work, 
+;; you need to have `markdown` installed system wide
+;; brew install markdown # on mac
+;; sudo apt install markdown # on GNU/Linux system
+
+(eval-after-load "markdown-mode"
+  '(progn
+     (define-key markdown-mode-map (kbd "<f5>") 'markdown-preview)))
+
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (provide 'init-markdown)
